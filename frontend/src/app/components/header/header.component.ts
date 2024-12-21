e600eee071e1f118cc5bf3a869dafbc6fe7123a0
Lorenzo <lmiscoli@gmail.com>
2024-12-21 08:52:08 +0100

    add header and common style

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component } from "@angular/core";
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
-  styleUrl: "./header.component.scss"
+  styleUrl: "./header.component.scss",
+  imports: [MatIconModule, MatButtonModule]
 })
 export class HeaderComponent {
-
 }
