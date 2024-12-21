131fb5dbc6190dfe63f0c55ef74f68fd4542c103
Lorenzo <lmiscoli@gmail.com>
2024-12-21 13:31:46 +0100

    create home component

---
+++ frontend/src/app/layouts/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+import { HOME_DEPS } from "./home.dependencies";
+
+@Component({
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.scss",
+  imports:[HOME_DEPS]
+})
+export default class HomeComponent {
+
+}
---
+++ frontend/src/app/layouts/home/home.dependencies.ts
@@ -0,0 +1,6 @@
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+export const HOME_DEPS = [MatInputModule, MatIconModule, MatSelectModule, MatFormFieldModule]
