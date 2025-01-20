f864cba79b542da5413b452d2dea1426a5ea8069
Lorenzo <lmiscoli@gmail.com>
2025-01-20 07:46:52 +0100

    add default use img

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,13 +1,13 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
-  styleUrl: "./header.component.scss",
-  imports: [MatButtonModule, MatIconModule]
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  imports: [MatButtonModule, MatIconModule, RouterLink],
 })
-export class HeaderComponent {
-}
+export class HeaderComponent {}
