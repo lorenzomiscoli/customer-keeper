908e1857ae4586bb485787df4a90c5340292987e
Lorenzo <lmiscoli@gmail.com>
2025-01-21 07:42:23 +0100

    add new customer button

--- frontend/src/app/layouts/home/components/search-filter/search-filter.dependencies.ts
+++ frontend/src/app/layouts/home/components/search-filter/search-filter.dependencies.ts
@@ -1,5 +1,6 @@
 import { FormsModule } from '@angular/forms';
 
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -11,4 +12,5 @@ export const SEARCH_FILTER_DEPS = [
   MatIconModule,
   MatSelectModule,
   MatFormFieldModule,
+  MatButtonModule
 ];
