4529f07aa943eb80593477fec938b54d999c064f
Lorenzo <lmiscoli@gmail.com>
2025-03-02 09:01:15 +0100

    add breadcrumb

--- frontend/src/app/layouts/customer/components/customer-add/customer-add.dependencies.ts
+++ frontend/src/app/layouts/customer/components/customer-add/customer-add.dependencies.ts
@@ -1,5 +1,6 @@
 import { AsyncPipe } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -13,6 +14,7 @@ import { ImageUploadComponent } from '../../../../components/image-upload/image-
 
 export const CUSTOMER_ADD_DEPS = [
   AsyncPipe,
+  RouterLink,
   ReactiveFormsModule,
   MatButtonModule,
   MatFormFieldModule,
--- frontend/src/app/layouts/customer/components/customer-details/customer-details.dependencies.ts
+++ frontend/src/app/layouts/customer/components/customer-details/customer-details.dependencies.ts
@@ -1,3 +1,5 @@
+import { RouterLink } from '@angular/router';
+
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { TranslocoDirective } from '@jsverse/transloco';
@@ -5,6 +7,7 @@ import { TranslocoDirective } from '@jsverse/transloco';
 import { CustomerEditComponent } from '../tabs/customer-edit/customer-edit.component';
 
 export const CUSTOMER_DETAILS_DEPS = [
+  RouterLink,
   MatTabsModule,
   MatIconModule,
   CustomerEditComponent,
