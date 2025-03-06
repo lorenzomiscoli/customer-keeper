710425da1ddf58bd9c74629231ae0e6c0e2194de
Lorenzo <lmiscoli@gmail.com>
2025-03-06 09:43:51 +0100

    remove about-dialog and version

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 
-import { MatDialog } from '@angular/material/dialog';
 import { AuthenticationService } from '../../services/authentication.service';
-import { AboutDialogComponent } from '../about-dialog/about-dialog.component';
 import { HEADER_DEPS } from './header.dependencies';
 
 @Component({
@@ -12,17 +10,9 @@ import { HEADER_DEPS } from './header.dependencies';
   imports: [HEADER_DEPS],
 })
 export class HeaderComponent {
-  constructor(
-    private authService: AuthenticationService,
-    private dialog: MatDialog
-  ) {}
+  constructor(private authService: AuthenticationService) {}
 
   public logout(): void {
     this.authService.logout();
   }
-
-  public openAboutDialog(): void {
-    this.dialog.open(AboutDialogComponent,{ panelClass: "about-dialog" });
-  }
-
 }
